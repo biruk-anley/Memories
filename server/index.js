19fd2c056bca057bb7cfaab4962b5bd661514080
biruk_anley <birukanley2022@gmail.com>
2022-12-22 19:57:55 +0300

    updating the server

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors';
 
+import PostRoutes from './routes/posts.js';
 const app = express()
 
 app.use(bodyParser.json({limit:"30mb", extended:true}))
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 app.use(cors());
+
+app.use('/posts', PostRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://kibur:1234abcd@clusters.cycdq7c.mongodb.net/?retryWrites=true&w=majority'
 
