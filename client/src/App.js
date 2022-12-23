dba8a6c52c80661e6e26ad7bfa8faf23d7b4abcf
biruk_anley <birukanley2022@gmail.com>
2022-12-23 22:36:57 +0300

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
+import Hasab from './images/Yehasabekub.png'
 
 const App = () => {
     return (
-        <div>
-            <h1>
-                Hello world
-            </h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center"  >
+                    የ ያሃብ እቁብ
+                </Typography>
+                <img src={ Hasab} alt='books' height={60}/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+
+                        <Grid item xs={12} sm={4}>
+                            <Forms/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
