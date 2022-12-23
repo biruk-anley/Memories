fbc10322078f6c6256f6dbcd9bec356defdc7766
biruk_anley <birukanley2022@gmail.com>
2022-12-23 22:37:01 +0300

    update form

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from ''
+const Post = () => {
+    return (
+        <>
+            <h1>
+            Post
+        </h1>
+        </>
+        
+    )
+}
+
+export default Post;
